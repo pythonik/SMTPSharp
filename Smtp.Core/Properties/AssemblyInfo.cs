c7fd0e44822c490c879c0fabb5287fc40bcee236
Kun Su <kunsupython@gmail.com>
2016-07-23 22:22:53 -0500

    Fixed Assembly info left from previous renaming

--- Smtp.Core/Properties/AssemblyInfo.cs
+++ Smtp.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Smtp.Core")]
+[assembly: AssemblyTitle("Smtp.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Smtp.Core")]
+[assembly: AssemblyProduct("Smtp.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
